5792c63864fcc554a8ac6a9238a7b396337b995e
Derek <dluu@trewknowledge.com>
2021-11-23 21:53:45 -0500

    I finally understand the closure created in debounce and how its called inside useCallback

--- utils/utils.js
+++ utils/utils.js
@@ -5,9 +5,10 @@ import { useCallback } from "react";
 
 // timers will not be cleared properly without useCallback
 export const debounce = (func, delay = 500) => {
-  // initialize timer out of scope of returning function otherwise it would be reinitialized every time the returning function runs and not contain the correct timeout id
+  // initialize timer out of scope of returning function otherwise it would be reinitialized every time the returning function runs and not contain the correct timeout id - this is why a closure is created
   let timer;
-  // note** why do we need return here? What is the difference between a callback function and a annonymous function that gets passed to useCallback?
+  // it is important to note that debounce is a callback function being passed to useCallback. Therefore it can be called inside of useCallback. Unlike an annoymous function which cannot be called inside useCallback, only passed as an argument
+  // the returned function is what is being passed to useCallback since debounce is called inside of useCallback (see illustration below)
   return (...args) => {
     // clears previous setTimeout as this function will be called frequently. it prevents callback from being called by previous timers, there should only be one active timer at a time and called only after the delay
     clearTimeout(timer);
@@ -16,6 +17,12 @@ export const debounce = (func, delay = 500) => {
     }, delay)
   }
 }
+
+// Illustration of useCallback
+const callback = debounce(() => {
+  console.log('do something')
+}, 1000)
+const someFunction = useCallback(callback)
 
 // when passed to usecallback
 // useCallback(() => {
